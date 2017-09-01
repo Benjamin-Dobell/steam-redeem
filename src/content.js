735c790804a9b99b319269e947b55619d85fec79
Benjamin Dobell <benjamin.dobell+git@glassechidna.com.au>
2017-09-02 01:21:56 +1000

    Handle Humble Bundle Steam keys appearing dynamically

--- src/content.js
+++ src/content.js
@@ -20,92 +20,96 @@ function redeemPressed(event) {
 
 function setupLargeRedeemButton(title, key, node, state=null) {
     if (state === null) {
-        state = parseInt(node.getAttribute('data-redemption-state')) || RedemptionState.DEFAULT
+        state = node.getAttribute('data-redemption-state') || RedemptionState.DEFAULT
     }
 
-    node.classList.add('steam-redeem-large-button')
+    if (state !== node.getAttribute('data-redemption-state')) {
+        node.classList.add('steam-redeem-large-button')
 
-    if (title) {
-        node.setAttribute('data-title', title)
+        if (title) {
+            node.setAttribute('data-title', title)
+        }
+
+        node.setAttribute('data-key', key)
+        node.setAttribute('data-redemption-state', state)
+
+        node.style.fontSize = '16px'
+        node.style.padding = '4px'
+        node.style.backgroundColor = '#000'
+        node.style.color = '#fff'
+        node.style.borderRadius = '4px'
+        node.style.fontWeight = '500'
+        node.style.cursor = 'pointer'
+
+        switch (state) {
+            default:
+            case RedemptionState.DEFAULT:
+                node.innerHTML = "<i class='hb hb-steam' style='margin-right: 8px'></i>Redeem Now!"
+                break
+
+            case RedemptionState.REDEEMING:
+                node.innerHTML = "<i class='hb hb-spinner hb-spin' style='margin-right: 8px'></i>Redeeming"
+                break
+
+            case RedemptionState.REDEEMED:
+                node.innerHTML = "<i class='hb hb-check' style='margin-right: 8px'></i>Redeemed"
+                break
+        }
+
+        node.addEventListener('click', redeemPressed)
     }
-
-    node.setAttribute('data-key', key)
-    node.setAttribute('data-redemption-state', state)
-
-    node.style.fontSize = '16px'
-    node.style.padding = '4px'
-    node.style.backgroundColor = '#000'
-    node.style.color = '#fff'
-    node.style.borderRadius = '4px'
-    node.style.fontWeight = '500'
-    node.style.cursor = 'pointer'
-
-    switch (state) {
-        default:
-        case RedemptionState.DEFAULT:
-            node.innerHTML = "<i class='hb hb-steam' style='margin-right: 8px'></i>Redeem Now!"
-            break
-
-        case RedemptionState.REDEEMING:
-            node.innerHTML = "<i class='hb hb-spinner hb-spin' style='margin-right: 8px'></i>Redeeming"
-            break
-
-        case RedemptionState.REDEEMED:
-            node.innerHTML = "<i class='hb hb-check' style='margin-right: 8px'></i>Redeemed"
-            break
-    }
-
-    node.addEventListener('click', redeemPressed)
 }
 
 function setupSmallRedeemButton(title, key, node, state=null) {
     if (state === null) {
-        state = parseInt(node.getAttribute('data-redemption-state')) || RedemptionState.DEFAULT
+        state = node.getAttribute('data-redemption-state') || RedemptionState.DEFAULT
     }
 
-    node.classList.add('steam-redeem-small-button')
+    if (state !== node.getAttribute('data-redemption-state')) {
+        node.classList.add('steam-redeem-small-button')
 
-    if (title) {
-        node.setAttribute('data-title', title)
+        if (title) {
+            node.setAttribute('data-title', title)
+        }
+
+        node.setAttribute('data-key', key)
+        node.setAttribute('data-redemption-state', state)
+
+        node.style.fontSize = '12px'
+        node.style.marginTop = '6px'
+        node.style.marginBottom = '6px'
+        node.style.marginLeft = '8px'
+        node.style.lineHeight = '24px'
+        node.style.paddingTop = '4px'
+        node.style.paddingBottom = '4px'
+        node.style.paddingLeft = '6px'
+        node.style.paddingRight = '6px'
+        node.style.backgroundColor = '#000'
+        node.style.color = '#fff'
+        node.style.borderRadius = '4px'
+        node.style.fontWeight = '600'
+        node.style.cursor = 'pointer'
+
+        switch (state) {
+            default:
+            case RedemptionState.DEFAULT:
+                node.innerHTML = "<i class='hb hb-steam' style='margin-right: 4px'></i>Redeem"
+                break
+
+            case RedemptionState.REDEEMING:
+                node.innerHTML = "<i class='hb hb-spinner hb-spin' style='margin-right: 4px'></i>Redeeming"
+                break
+
+            case RedemptionState.REDEEMED:
+                node.innerHTML = "<i class='hb hb-check' style='margin-right: 4px'></i>Redeemed"
+                break
+        }
+
+        node.addEventListener('click', redeemPressed)
     }
-
-    node.setAttribute('data-key', key)
-    node.setAttribute('data-redemption-state', state)
-
-    node.style.fontSize = '12px'
-    node.style.marginTop = '6px'
-    node.style.marginBottom = '6px'
-    node.style.marginLeft = '8px'
-    node.style.lineHeight = '24px'
-    node.style.paddingTop = '4px'
-    node.style.paddingBottom = '4px'
-    node.style.paddingLeft = '6px'
-    node.style.paddingRight = '6px'
-    node.style.backgroundColor = '#000'
-    node.style.color = '#fff'
-    node.style.borderRadius = '4px'
-    node.style.fontWeight = '600'
-    node.style.cursor = 'pointer'
-
-    switch (state) {
-        default:
-        case RedemptionState.DEFAULT:
-            node.innerHTML = "<i class='hb hb-steam' style='margin-right: 4px'></i>Redeem"
-            break
-
-        case RedemptionState.REDEEMING:
-            node.innerHTML = "<i class='hb hb-spinner hb-spin' style='margin-right: 4px'></i>Redeeming"
-            break
-
-        case RedemptionState.REDEEMED:
-            node.innerHTML = "<i class='hb hb-check' style='margin-right: 4px'></i>Redeemed"
-            break
-    }
-
-    node.addEventListener('click', redeemPressed)
 }
 
-function findKeys() {
+function processKeys() {
     const largeButtonKeys = []
 
     let containerNodes = document.evaluate("//*[contains(concat(' ', normalize-space(@class), ' '), ' sr-key ') and .//*[contains(@class, 'sr-redeemed')]]", document, null, XPathResult.ANY_TYPE, null)
@@ -114,15 +118,15 @@ function findKeys() {
     while (containerNode) {
         let keyNode = document.evaluate(".//*[@class='sr-redeemed']", containerNode, null, XPathResult.ANY_TYPE, null).iterateNext()
         let titleNode = document.evaluate(".//*[@class='sr-key-heading']", containerNode, null, XPathResult.ANY_TYPE, null).iterateNext()
-        let instructionsNode = document.evaluate(".//*[@class='redeem-instructions']", containerNode, null, XPathResult.ANY_TYPE, null).iterateNext()
+        let instructionsNode = document.evaluate(".//*[contains(@class, 'redeem-instructions') and (contains(@class, 'steam-redeem-large-button') or .//*[contains(text(),'Copy this key')])]", containerNode, null, XPathResult.ANY_TYPE, null).iterateNext()
 
         if (keyNode && titleNode && instructionsNode) {
             const title = titleNode.textContent.trim()
             const key = keyNode.textContent.trim().split('\n')[0]
 
             largeButtonKeys.push({
-                title: titleNode.textContent.trim(),
-                key: key,
+                title,
+                key,
                 node: instructionsNode
             })
         }
@@ -136,8 +140,8 @@ function findKeys() {
     containerNode = containerNodes.iterateNext()
 
     while (containerNode) {
-        let keyNode = document.evaluate(".//*[contains(@class, 'redeemed')]", containerNode, null, XPathResult.ANY_TYPE, null).iterateNext()
-        let titleNode = document.evaluate("./*[@class='game-name']", containerNode, null, XPathResult.ANY_TYPE, null).iterateNext()
+        const keyNode = document.evaluate(".//*[contains(@class, 'redeemed')]", containerNode, null, XPathResult.ANY_TYPE, null).iterateNext()
+        const titleNode = document.evaluate("./*[@class='game-name']", containerNode, null, XPathResult.ANY_TYPE, null).iterateNext()
 
         if (keyNode && titleNode) {
             const key = keyNode.textContent.trim()
@@ -170,12 +174,36 @@ function findKeys() {
         }
     })
 
-    return smallButtonKeys.concat(largeButtonKeys)
+    const keys = smallButtonKeys.concat(largeButtonKeys)
+    chrome.runtime.sendMessage({ keysFound: keys.length > 0 })
+    return keys
 }
+
+function attachObservers() {
+    let containerNodes = document.evaluate("//*[contains(concat(' ', normalize-space(@class), ' '), ' sr-widget-key-list-region ')]", document, null, XPathResult.ANY_TYPE, null)
+    let containerNode = containerNodes.iterateNext()
+
+    while (containerNode) {
+        newKeyObserver.observe(containerNode, {subtree: true, attributes: true, childList: true})
+        containerNode = containerNodes.iterateNext()
+    }
+
+    containerNodes = document.evaluate("//*[contains(concat(' ', normalize-space(@class), ' '), ' unredeemed-keys-table ')]", document, null, XPathResult.ANY_TYPE, null)
+    containerNode = containerNodes.iterateNext()
+
+    while (containerNode) {
+        newKeyObserver.observe(containerNode, {subtree: true, attributes: true, childList: true})
+        containerNode = containerNodes.iterateNext()
+    }
+}
+
+let newKeyObserver = new MutationObserver((mutations) => {
+    processKeys()
+})
 
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
     if (request.redeemAll) {
-        const keys = findKeys()
+        const keys = processKeys()
         chrome.runtime.sendMessage({redeem: keys.map(entry => ({title: entry.title, key: entry.key}))})
     }
 
@@ -191,8 +219,9 @@ chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
     }
 })
 
-for (let timeout of [100, 750, 3000, 10000]) {
+for (let timeout of [100, 750, 3000]) {
     setTimeout(function() {
-        chrome.runtime.sendMessage({ keysFound: findKeys().length > 0 })
+        attachObservers()
+        processKeys()
     }, timeout)
 }
